0ac78ea2cc90c02d62642a6ffadc41b672a1ad84
facutk <facu.tk@gmail.com>
2015-08-09 18:16:12 -0300

    added morse example

--- errors/cleaner_script.py
+++ errors/cleaner_script.py
@@ -1,8 +1,8 @@
-lines = open("real_world_data", "rb").readlines()
+lines = open("rwmorse", "rb").readlines()
 parsed = []
 for line in lines:
     parsed.append( bin(int(line,16)).replace('0b','').zfill(32)[16:] )
 
-fout = open("limpio.txt", "wb")
+fout = open("morse_limpio.txt", "wb")
 fout.write( "\n".join(parsed) )
 fout.close()
