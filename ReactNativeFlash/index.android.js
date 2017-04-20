68b5ff786f1c4bdbf5d1e1dc9aa7a7303a30f45c
jtkaczyszyn <jtkaczyszyn@elementum.com>
2017-04-20 15:17:35 -0300

    add android app

---
+++ ReactNativeFlash/index.android.js
@@ -0,0 +1,114 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import RNFlash from 'react-native-flash';
+
+RNFlash.turnOnFlash(); // turn on flash
+		
+RNFlash.turnOffFlash(); // turn off flash
+		
+RNFlash.hasFlash(function(){
+  //RNFlash.turnOnFlash();
+},function(){
+  alert("You do not have flash")
+});
+
+export default class AwesomeProject extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      flash: false,
+      profile: 'Profile'
+    };
+  }
+
+  handleFlashToggle = () => {
+    this.setState({
+      flash: !this.state.flash
+    })
+
+    if (this.state.flash) {
+      RNFlash.turnOnFlash();
+    } else {
+      RNFlash.turnOffFlash();
+    }
+  }
+
+  handleProfile = () => {
+    this.setState({
+      profile: 'Profiling...'
+    })
+
+    const start_time = new Date();
+    const max_loops = 100;
+    for(var i = 0; i < max_loops; i++) {
+      RNFlash.turnOnFlash();
+      RNFlash.turnOffFlash();
+    }
+    const end_time = new Date();
+    const profile_time = end_time - start_time;
+
+    this.setState({
+      profile: `${max_loops} loops took ${profile_time}ms`
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Elementum
+        </Text>
+        <Text style={styles.instructions}>
+          react-native
+        </Text>
+        <Button
+          onPress={this.handleFlashToggle}
+          title="Toggle Flash"
+          color="#841584"
+        />
+        <Button
+          onPress={this.handleProfile}
+          title={this.state.profile}
+          color="#841584"
+        />
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
